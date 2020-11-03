ee4c90819df5ae8dc516774b0c10eaf74340f133
Hanno Braun <hanno@braun-embedded.com>
2020-11-03 15:59:01 +0100

    Generate `StepMode256` in macro
    This makes it very straight-forward to generate additional step mode
    enums.

--- src/step_mode.rs
+++ src/step_mode.rs
@@ -1,60 +1,118 @@
 use core::convert::TryFrom;
 
-/// Defines the step mode with a resolution of up to 256 microsteps
-#[derive(Clone, Copy, Debug, Eq, PartialEq, Ord, PartialOrd)]
-pub enum StepMode256 {
-    /// Full steps
-    Full = 1,
+use paste::paste;
 
-    /// 2 microsteps per full step
-    M2 = 2,
+macro_rules! generate_step_mode_enums {
+    (
+        $(
+            $max:expr => $($variant:expr),*;
+        )*
+    ) => {
+        $(
+            generate_step_mode_enums!(@gen_enum, (), (), $max => $($variant,)*);
+        )*
+    };
 
-    /// 4 microsteps per full step
-    M4 = 4,
+    // This is a trick to work around a limitation of Rust macros: We can't
+    // generate a part of something, like an enum variant. We can only generate
+    // complete things, like the whole enum.
+    //
+    // This first rules gets matched on the first call, when there is not output
+    // yet. It is generates the full step variant, then passes the input on to
+    // the next macro call.
+    (
+        @gen_enum,
+        (),
+        (),
+        $max:expr => $($input:expr,)*
+    ) => {
+        generate_step_mode_enums!(
+            @gen_enum,
+            (
+                #[doc = "Full steps"]
+                Full = 1,
+            ),
+            (
+                1 => Ok(Self::Full),
+            ),
+            $max => $($input,)*
+        );
+    };
+    // This next rule gets matched as long as there are still enum variants to
+    // be generated. It creates the tokens that make up a variant, then passes
+    // them and the rest of the input on to the next recursive macro call.
+    (
+        @gen_enum,
+        (
+            $($variant_output:tt)*
+        ),
+        (
+            $($try_from_output:tt)*
+        ),
+        $max:expr => $variant:expr, $($input:expr,)*
+    ) => {
+        generate_step_mode_enums!(
+            @gen_enum,
+            (
+                $($variant_output)*
 
-    /// 8 microsteps per full step
-    M8 = 8,
+                #[doc = $variant " microsteps per full step"]
+                [<M $variant>] = $variant,
+            ),
+            (
+                $($try_from_output)*
 
-    /// 16 microsteps per full step
-    M16 = 16,
+                $variant => Ok(Self::[<M $variant>]),
+            ),
+            $max => $($input,)*
+        );
+    };
+    // This last rule gets matched when there is no more input left and all
+    // variants have been generated. It takes all the tokens generated in
+    // previous macro calls and uses them to generate the complete enum.
+    (
+        @gen_enum,
+        (
+            $($variant_output:tt)*
+        ),
+        (
+            $($try_from_output:tt)*
+        ),
+        $max:expr =>
+    ) => {
+        paste! {
+            #[doc =
+                "Defines the step mode with a resolution of up to " $max " \
+                microsteps"
+            ]
+            #[derive(Clone, Copy, Debug, Eq, PartialEq, Ord, PartialOrd)]
+            pub enum [<StepMode $max>] {
+                $($variant_output)*
+            }
 
-    /// 32 microsteps per full step
-    M32 = 32,
+            impl From<[<StepMode $max>]> for u16 {
+                fn from(step_mode: [<StepMode $max>]) -> Self {
+                    step_mode as Self
+                }
+            }
 
-    /// 64 microsteps per full step
-    M64 = 64,
+            impl TryFrom<u16> for [<StepMode $max>] {
+                type Error = InvalidStepModeError;
 
-    /// 128 microsteps per full step
-    M128 = 128,
+                fn try_from(val: u16) -> Result<Self, Self::Error> {
+                    match val {
+                        $($try_from_output)*
 
-    /// 256 microsteps per full step
-    M256 = 256,
-}
-
-impl From<StepMode256> for u16 {
-    fn from(step_mode: StepMode256) -> Self {
-        step_mode as Self
-    }
-}
-
-impl TryFrom<u16> for StepMode256 {
-    type Error = InvalidStepModeError;
-
-    fn try_from(val: u16) -> Result<Self, Self::Error> {
-        match val {
-            1 => Ok(StepMode256::Full),
-            2 => Ok(StepMode256::M2),
-            4 => Ok(StepMode256::M4),
-            8 => Ok(StepMode256::M8),
-            16 => Ok(StepMode256::M16),
-            32 => Ok(StepMode256::M32),
-            64 => Ok(StepMode256::M64),
-            128 => Ok(StepMode256::M128),
-            256 => Ok(StepMode256::M256),
-
-            _ => Err(InvalidStepModeError),
+                        _ => Err(InvalidStepModeError),
+                    }
+                }
+            }
         }
-    }
+    };
+}
+
+generate_step_mode_enums! {
+    256 => 2, 4, 8, 16, 32, 64, 128, 256;
 }
 
 /// Indicates that a given step mode value did not represent a valid step mode
