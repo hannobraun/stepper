3a26b7af7cd7a69212ced19ed0d873ba4930054b
Hanno Braun <hanno@braun-embedded.com>
2020-11-20 16:30:20 +0100

    Verify stepper motor movement in test suite

--- test-stand/src/lib.rs
+++ test-stand/src/lib.rs
@@ -6,6 +6,7 @@ use panic_probe as _; // panic handler
 // Re-export dependencies, so test suite can use them.
 pub extern crate defmt;
 pub extern crate lpc8xx_hal;
+pub extern crate rotary_encoder_hal;
 pub extern crate step_dir;
 
 use lpc8xx_hal::cortex_m::asm;
--- test-stand/tests/tests/stspin220.rs
+++ test-stand/tests/tests/stspin220.rs
@@ -8,12 +8,17 @@ use core::fmt::Debug;
 use test_stand::{
     lpc8xx_hal::{
         self,
-        gpio::{self, direction::Output, GpioPin},
+        gpio::{
+            self,
+            direction::{Input, Output},
+            GpioPin,
+        },
         mrt::{self, MRT0},
-        pins::{PIO0_16, PIO0_17, PIO0_18, PIO0_19, PIO0_20},
+        pins::{PIO0_0, PIO0_1, PIO0_16, PIO0_17, PIO0_18, PIO0_19, PIO0_20},
     },
+    rotary_encoder_hal::{Direction, Rotary},
     step_dir::{
-        embedded_hal::timer,
+        embedded_hal::{digital::InputPin, timer},
         embedded_time::{clock::Clock, duration::Microseconds},
         stspin220::STSPIN220,
         Dir, Step, StepMode256,
@@ -30,13 +35,14 @@ struct Context {
         GpioPin<PIO0_20, Output>,
     >,
     timer: mrt::Channel<MRT0>,
+    rotary: Rotary<GpioPin<PIO0_0, Input>, GpioPin<PIO0_1, Input>>,
 }
 
 #[defmt_test::tests]
 mod tests {
     #[init]
     fn init() -> super::Context {
-        use super::{gpio, lpc8xx_hal, mrt, StepMode256, STSPIN220};
+        use super::{gpio, lpc8xx_hal, mrt, Rotary, StepMode256, STSPIN220};
 
         let p = lpc8xx_hal::Peripherals::take().unwrap();
 
@@ -65,6 +71,9 @@ mod tests {
             .pio0_20
             .into_output_pin(gpio.tokens.pio0_20, gpio::Level::Low);
 
+        let rotary_a = p.pins.pio0_0.into_input_pin(gpio.tokens.pio0_0);
+        let rotary_b = p.pins.pio0_1.into_input_pin(gpio.tokens.pio0_1);
+
         let mut timer = mrt.mrt0;
 
         timer.start(mrt::MAX_VALUE);
@@ -78,42 +87,149 @@ mod tests {
             )
             .unwrap();
 
-        super::Context { driver, timer }
+        let rotary = Rotary::new(rotary_a, rotary_b);
+
+        super::Context {
+            driver,
+            timer,
+            rotary,
+        }
     }
 
     #[test]
     fn test_step(cx: &mut super::Context) {
-        super::test_step(&mut cx.driver, &mut cx.timer);
+        super::test_step(&mut cx.driver, &mut cx.timer, &mut cx.rotary);
     }
 }
 
-fn test_step<Driver, Timer>(driver: &mut Driver, timer: &mut Timer)
-where
+fn test_step<Driver, Timer, A, B>(
+    driver: &mut Driver,
+    timer: &mut Timer,
+    rotary: &mut Rotary<A, B>,
+) where
     Driver: Step,
     Driver::Error: Debug,
     Timer: timer::CountDown<Time = u32> + Clock,
     Timer::Error: Debug,
+    A: InputPin,
+    A::Error: Debug,
+    B: InputPin,
+    B::Error: Debug,
 {
-    verify_steps(driver, timer, Dir::Forward);
-    verify_steps(driver, timer, Dir::Backward);
+    verify_steps(driver, timer, rotary, Dir::Forward);
+    verify_steps(driver, timer, rotary, Dir::Backward);
 }
 
-fn verify_steps<Driver, Timer>(
+fn verify_steps<Driver, Timer, A, B>(
     driver: &mut Driver,
     timer: &mut Timer,
+    rotary: &mut Rotary<A, B>,
     direction: Dir,
 ) where
     Driver: Step,
     Driver::Error: Debug,
     Timer: timer::CountDown<Time = u32> + Clock,
     Timer::Error: Debug,
+    A: InputPin,
+    A::Error: Debug,
+    B: InputPin,
+    B::Error: Debug,
 {
     const STEP_DELAY: Microseconds = Microseconds(10_000);
 
-    for _ in 0..20 {
-        timer.try_start(mrt::MAX_VALUE).unwrap();
-        let step_timer = timer.new_timer(STEP_DELAY).start().unwrap();
-        driver.step(direction, timer).unwrap();
-        step_timer.wait().unwrap();
+    // Stepper motor has 200 steps per revolution, encoder has 20 counts.
+    const STEPS_PER_COUNT: u32 = 10;
+
+    // Discard the first direction read by the rotary encoder. It starts out
+    // with an initial state that is constant, i.e. doesn't reflect the actual
+    // initial state. Unless the actual state happens to be the same by
+    // accident, it will mistake its initial reading for a first movement.
+    let _ = rotary.update().unwrap();
+
+    // Depending on the initial position of the rotary encoder's magnet, we
+    // might not read the number of encoder counts correctly.  If we start at a
+    // position where the encoder would count, then we'll end up at such a
+    // position again, after we're done rotating. That last count might or might
+    // not be detected.
+    //
+    // We can solve that by making sure we start out right in the middle between
+    // two such positions. Then we'll also end up in the middle at the end of
+    // the movement and can be sure all counts are detected.
+    //
+    // Here we step the motor until we read a count, then move half the number
+    // of steps that make up a count, to get us to a desired middle position.
+    while step(driver, timer, rotary, STEP_DELAY, direction, false) == 0 {}
+    for _ in 0..STEPS_PER_COUNT / 2 {
+        step(driver, timer, rotary, STEP_DELAY, direction, false);
     }
+
+    let steps = 20;
+    let counts_expected = steps / STEPS_PER_COUNT;
+
+    let mut counts = 0;
+    for _ in 0..steps {
+        counts += step(driver, timer, rotary, STEP_DELAY, direction, true);
+    }
+
+    defmt::info!(
+        "Encoder counts expected: {:?}; measured: {:?}",
+        counts_expected,
+        counts
+    );
+    assert_eq!(counts_expected, counts);
+}
+
+fn step<Driver, Timer, A, B>(
+    driver: &mut Driver,
+    timer: &mut Timer,
+    rotary: &mut Rotary<A, B>,
+    delay: Microseconds,
+    direction: Dir,
+    check_direction: bool,
+) -> u32
+where
+    Driver: Step,
+    Driver::Error: Debug,
+    Timer: timer::CountDown<Time = u32> + Clock,
+    Timer::Error: Debug,
+    A: InputPin,
+    A::Error: Debug,
+    B: InputPin,
+    B::Error: Debug,
+{
+    let expected_direction = match direction {
+        Dir::Forward => Direction::Clockwise,
+        Dir::Backward => Direction::CounterClockwise,
+    };
+
+    timer.try_start(mrt::MAX_VALUE).unwrap();
+    let step_timer = timer.new_timer(delay).start().unwrap();
+    driver.step(direction, timer).unwrap();
+
+    let mut counts = 0;
+
+    loop {
+        match rotary.update().unwrap() {
+            Direction::None => {}
+            direction if direction == expected_direction => {
+                counts += 1;
+            }
+            direction => {
+                // Depending on initial conditions, we can get an unexpected
+                // direction reading here. To prevent this during the test, the
+                // initial conditions are set up in a controlled way. The
+                // `check_direction` argument gives the caller the opportunity
+                // to disable this check, while this is done.
+                if check_direction {
+                    defmt::panic!("Unexpected direction: {:?}", direction);
+                }
+            }
+        }
+
+        if step_timer.is_expired().unwrap() {
+            break;
+        }
+    }
+
+    counts
 }
