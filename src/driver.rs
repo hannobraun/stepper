17fcae6f5b7b164bf4efe591fe9f2a82ffdb4fe6
Hanno Braun <hanno@braun-embedded.com>
2020-12-07 16:17:32 +0100

    Add the beginnings of a new abstract driver API

--- src/driver.rs
+++ src/driver.rs
@@ -1,5 +1,47 @@
 use embedded_time::TimeError;
 
+/// Abstract interface to stepper motor drivers
+///
+/// Wraps a concrete driver and uses the traits that the concrete driver
+/// implements to provide an abstract API.
+pub struct Driver<T> {
+    inner: T,
+}
+
+impl<T> Driver<T> {
+    /// Create a new `Driver` instance from a concrete driver
+    ///
+    /// Since `Driver` only provides an abstract interface for _using_ a driver,
+    /// not for initializing it, you have to initialize a concrete driver and
+    /// pass it to this constructor.
+    pub fn new(inner: T) -> Self {
+        Self { inner }
+    }
+
+    /// Access a reference to the wrapped driver
+    ///
+    /// Can be used to access driver-specific functionality that can't be
+    /// provided by `Driver`'s abstract interface.
+    pub fn inner(&self) -> &T {
+        &self.inner
+    }
+
+    /// Access a mutable reference to the wrapped driver
+    ///
+    /// Can be used to access driver-specific functionality that can't be
+    /// provided by `Driver`'s abstract interface.
+    pub fn inner_mut(&mut self) -> &mut T {
+        &mut self.inner
+    }
+
+    /// Release the wrapped driver
+    ///
+    /// Drops this instance of `Driver` and returns the wrapped driver.
+    pub fn release(self) -> T {
+        self.inner
+    }
+}
+
 /// An error that can occur while setting the microstepping mode
 #[derive(Debug, Eq, PartialEq)]
 pub enum ModeError<OutputPinError> {
