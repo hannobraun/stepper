191b56db4db80e02be9c9332b6297e764bc97cf9
Hanno Braun <hanno@braun-embedded.com>
2020-12-10 15:32:40 +0100

    Include `generate-drivers` task in CI build
    This runs a task, which seems weird, but also not really problematic. It
    would be better to just build it, of course, but it seems that is
    non-trivial. I've opened a task upstream:
    https://github.com/neonphog/cargo-task/issues/7
    
    Close #38

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,6 +4,10 @@ set -e
 # Fail build, if there are any warnings.
 export RUSTFLAGS="-D warnings"
 
+# Main library and drivers
 cargo build --verbose &&
 cargo test --verbose &&
-cargo doc
+cargo doc &&
+
+# generate-drivers task
+cargo task generate-drivers
